26e47917a4930e9ba823d080f93c8257f0d37ca6
jameseroni <dd8917vk@metrostate.edu>
2021-02-27 16:59:06 -0600

    styles

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 const fetchPosts = async () => {
-    // let response = await fetch('https://gist.githubusercontent.com/paulmillr/4524946/raw/c462a62ac9f3a072fc4106bbd131335ad730da16/github-users-stats.json');
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let response = await fetch('https://gist.githubusercontent.com/paulmillr/4524946/raw/c462a62ac9f3a072fc4106bbd131335ad730da16/github-users-stats.json');
+    // let response = await fetch('https://jsonplaceholder.typicode.com/posts');
     let data = await response.json();
     return data;
 }
